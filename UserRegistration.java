278b47713c10b4b38582297f82b8ca333165a4b3
SaiprasadPampatwar <pampatwarsaiprasad321@gmail.com>
2020-11-20 21:43:41 +0530

    validated last name

--- UserRegistration.java
+++ UserRegistration.java
@@ -5,15 +5,18 @@ public class UserRegistration {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to User Registration");
 		Scanner sc = new Scanner(System.in);
-	      System.out.println("Enter your first name: ");
-	      String name = sc.next();
-	      String regex = "[A-Z]{1}[a-zA-Z]{2,}";
-	      boolean result = name.matches(regex);
-	      if(result) {
-	         System.out.println("Given name is valid.");
-	      } else {
-	         System.out.println("Given name is not valid.");
-	      }
+        System.out.println("Enter your first name: ");
+        String fname = sc.next();
+        System.out.println("Enter your last name: ");
+        String lname = sc.next();
+        String regex = "[A-Z]{1}[a-zA-Z]{2,}";
+        boolean result = (fname.matches(regex) && lname.matches(regex));
+        if(result) {
+        	System.out.println("Given Name is valid.");
+        } 
+        else {
+         System.out.println("Given Name is not valid.");
+        }
 
 }
 }
