8260a73ff365775d65ecd744d4e8fe5ca0d8f8ff
SaiprasadPampatwar <pampatwarsaiprasad321@gmail.com>
2020-11-20 21:29:50 +0530

    As a User need to

--- UserRegistration.java
+++ UserRegistration.java
@@ -1,11 +1,20 @@
 import java.util.*;
-
-
 public class UserRegistration {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to User Registration");
-		
+		Scanner sc = new Scanner(System.in);
+	      System.out.println("Enter your first name: ");
+	      String name = sc.next();
+	      String regex = "[A-Z]{1}[a-zA-Z]{2,}";
+	      //Matching the given phone number with regular expression
+	      boolean result = name.matches(regex);
+	      if(result) {
+	         System.out.println("Given name is valid");
+	      } else {
+	         System.out.println("Given name is not valid");
+	      }
+
 }
 }
