48d01ed5312fab3ad592356a38b7babed7b989ed
SaiprasadPampatwar <pampatwarsaiprasad321@gmail.com>
2020-11-25 18:08:36 +0530

    [Saiprasad]Added: Parametrised test method for emailValidation

--- UserRegistrationTest.java
+++ UserRegistrationTest.java
@@ -1,9 +1,49 @@
 import static org.junit.Assert.*;
 
-import org.junit.Test;
+import java.util.Arrays;
+import java.util.Collection;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+@RunWith(Parameterized.class)
 public class UserRegistrationTest {
+	private String email;
+	private boolean result;
+	private UserRegistration userRegistration2;
 	
+	
+	@Before
+	public void initialize() {
+		userRegistration2 = new UserRegistration();
+	}
+	
+	public UserRegistrationTest(String email, boolean result) {
+		this.email = email;
+		this.result = result;
+	}
+	
+	
+	@Parameterized.Parameters
+	public static Collection emailChecker() {
+		return Arrays.asList(new Object[][] {{"abc@yahoo.com",true},{"abc-100@yahoo.com",true},{"abc.100@yahoo.com",true},
+			{"abc-100@abc.net",true},{"abc.100@abc.com.au",true},{"abc@1.com",true},{"abc@gmail.com.com",true},{"abc+100@gmail.com",true},
+			{"abc-",false},{"abc@.com",false},{"abc@123gmail.a",false},{"abc123@.com",false},
+			{"abc123@.com.com",false},{".abc@abc.com",false},{"abc()*@gmail.com",false},{"abc@%*.com.com",false},
+			{"abc..2002@.gmail.com",false},{"abc.@gmail.com",false},{"abc@abc@gmail.com",false},
+			{"abc@gmail.com.1a",false},{"abc@gmail.com.aa.au",false}
+		});
+	}
+	
+	
+	
+	@Test
+	public void testEmailsMultiple() {
+		assertEquals(result, userRegistration2.emailValidation(email));
+	}
+	
+	/*
 	UserRegistration userRegistration = new UserRegistration();
 	@Test
 	public void testFirstName_returnTrue() {
@@ -85,5 +125,5 @@ public class UserRegistrationTest {
 	public void Given_Message_When_Happy_Should_Return_Happy() {
 		String mood = userRegistration.analyseMood("This is a Happy Message");
 		assertEquals("HAPPY", mood);
-	}
+	}*/
 }
